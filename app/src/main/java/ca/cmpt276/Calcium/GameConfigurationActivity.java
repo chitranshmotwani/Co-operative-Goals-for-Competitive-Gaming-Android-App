63e5851aeb60039706a2d2b3702f34c0460a5b58
Jen <45193617+jengu288@users.noreply.github.com>
2022-10-31 19:44:09 -0700

    Initial Commit with Empty Activities

---
+++ app/src/main/java/ca/cmpt276/Calcium/GameConfigurationActivity.java
@@ -0,0 +1,14 @@
+package ca.cmpt276.Calcium;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GameConfigurationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_configuration);
+    }
+}
---
+++ app/src/main/java/ca/cmpt276/Calcium/NewGameConfigurationActivity.java
@@ -0,0 +1,14 @@
+package ca.cmpt276.Calcium;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewGameConfigurationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_game_configuration);
+    }
+}
