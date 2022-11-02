aab9b5531254cd4a48682c8b878f3b9083d68e17
Jen <45193617+jengu288@users.noreply.github.com>
2022-11-01 20:58:02 -0700

    Add automatic score calculation for each game added to a game configuration

--- app/src/main/java/ca/cmpt276/Calcium/model/GameConfiguration.java
+++ app/src/main/java/ca/cmpt276/Calcium/model/GameConfiguration.java
@@ -1,19 +1,32 @@
 package ca.cmpt276.Calcium.model;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 public class GameConfiguration {
     private ArrayList<Game> gameList = new ArrayList<>();
     private String name = null;
+    private String scoreSystemDescription = null;
     private int highPerPlayerScore = 0;
     private int lowPerPlayerScore = 0;
 
-    public ArrayList<Game> getGameList() {
-        return gameList;
+    public enum AchievementLevel {
+        LEVEL_1, //highest score
+        LEVEL_2,
+        LEVEL_3,
+        LEVEL_4,
+        LEVEL_5,
+        LEVEL_6,
+        LEVEL_7,
+        LEVEL_8,
+        LEVEL_9 //lowest score*/
     }
 
-    public void setGameList(ArrayList<Game> gameList) {
-        this.gameList = gameList;
+    public GameConfiguration(String name, String scoreSystemDescription, int highPerPlayerScore, int lowPerPlayerScore) {
+        this.name = name;
+        this.scoreSystemDescription = scoreSystemDescription;
+        this.highPerPlayerScore = highPerPlayerScore;
+        this.lowPerPlayerScore = lowPerPlayerScore;
     }
 
     public String getName() {
@@ -22,6 +35,14 @@ public class GameConfiguration {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getScoreSystemDescription() {
+        return scoreSystemDescription;
+    }
+
+    public void setScoreSystemDescription(String description) {
+        this.scoreSystemDescription = description;
     }
 
     public int getHighPerPlayerScore() {
@@ -40,9 +61,83 @@ public class GameConfiguration {
         this.lowPerPlayerScore = lowPerPlayerScore;
     }
 
+    public Game getGame(int index) {
+        return gameList.get(index);
+    }
+
+    public void addGame(int numPlayers, int score) {
+        int achievementLevelIndex = calculateAchievementLevel(numPlayers, score);
+        AchievementLevel levelObtained = AchievementLevel.values()[achievementLevelIndex];
+
+        gameList.add(new Game(LocalDateTime.now(), numPlayers, score, levelObtained));
+    }
+
+    private int calculateAchievementLevel(int numPlayers, int score) {
+        float scaledHighScore = highPerPlayerScore * numPlayers;
+        float scaledLowScore = lowPerPlayerScore * numPlayers;
+        if(score < scaledLowScore) {
+            //return the ranking that is below the expected low score
+            return AchievementLevel.values().length - 1;
+        }else if(score > scaledHighScore) {
+            //return highest rank possible
+            return 0;
+        }
+
+        //top and bottom tiers already accounted for
+        float interval = (scaledHighScore - scaledLowScore) / ((AchievementLevel.values().length) - 2);
+        int index = 0;
+        for (int i = 0; i < AchievementLevel.values().length; i++) {
+            if (score < (scaledLowScore + (interval * i))) {
+                index = i + 1;
+                break;
+            }
+        }
+
+        //since the ranking is highest to lowest
+        return AchievementLevel.values().length - index;
+    }
+
     public class Game {
+        private LocalDateTime dateTimeCreated;
         private int numPlayers = 0;
         private int score = 0;
+        private AchievementLevel achievementLevel;
 
+        public Game(LocalDateTime dateTimeCreated, int numPlayers, int score, AchievementLevel achievementLevel) {
+            //no setter for dateTimeCreated as it should only be set on the game creation
+            this.dateTimeCreated = dateTimeCreated;
+            this.numPlayers = numPlayers;
+            this.score = score;
+            this.achievementLevel = achievementLevel;
+        }
+
+        public LocalDateTime getDateTimeCreated() {
+            return dateTimeCreated;
+        }
+
+
+        public int getNumPlayers() {
+            return numPlayers;
+        }
+
+        public void setNumPlayers(int numPlayers) {
+            this.numPlayers = numPlayers;
+        }
+
+        public int getScore() {
+            return score;
+        }
+
+        public void setScore(int score) {
+            this.score = score;
+        }
+
+        public AchievementLevel getAchievementLevel() {
+            return achievementLevel;
+        }
+
+        public void setAchievementLevel(AchievementLevel achievementLvl) {
+            achievementLevel = achievementLvl;
+        }
     }
 }
