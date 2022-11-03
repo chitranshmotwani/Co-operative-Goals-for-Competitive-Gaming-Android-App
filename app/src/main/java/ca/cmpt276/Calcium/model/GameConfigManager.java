4a62feb411bb24f0a26d0242b14afe66bfa79ba2
Weipeng Yao <weipengy@sfu.ca>
2022-11-03 07:13:11 -0700

    1.0 Add basic methods

--- app/src/main/java/ca/cmpt276/Calcium/model/GameConfigManager.java
+++ app/src/main/java/ca/cmpt276/Calcium/model/GameConfigManager.java
@@ -1,4 +1,27 @@
 package ca.cmpt276.Calcium.model;
 
+import java.util.ArrayList;
+
 public class GameConfigManager {
+    private static GameConfigManager manager;
+    private GameConfigManager(){}
+
+    public static synchronized GameConfigManager getInstance(){
+        if (manager == null) {
+            manager = new GameConfigManager();
+        }
+        return manager;
+    }
+
+    private ArrayList<GameConfiguration> configs = new ArrayList<>();
+
+    public GameConfiguration getConfig(int index)   { return configs.get(index); }
+
+    public void setConfig(int index, GameConfiguration newConfig)  { configs.set(index,newConfig); }
+
+    public void addConfig(GameConfiguration newConfig)  { configs.add(newConfig); }
+
+    public int getNumOfConfigs()    { return configs.size(); }
+
+    
 }
