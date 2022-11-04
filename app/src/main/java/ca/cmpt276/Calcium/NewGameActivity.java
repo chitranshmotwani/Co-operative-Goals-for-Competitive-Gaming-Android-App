3b5d130a1327f89b1b6602ca524e13f04ad66a6e
cma115 <chitransh_motwani@sfu.ca>
2022-11-04 16:48:27 -0700

    Committing NewGameActivity and GameListActivity

--- app/src/main/java/ca/cmpt276/Calcium/NewGameActivity.java
+++ app/src/main/java/ca/cmpt276/Calcium/NewGameActivity.java
@@ -3,12 +3,22 @@ package ca.cmpt276.Calcium;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class NewGameActivity extends AppCompatActivity {
+
+    private Menu optionsMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_game);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.save_menu, menu);
+        optionsMenu = menu;
+        return true;
     }
 }
