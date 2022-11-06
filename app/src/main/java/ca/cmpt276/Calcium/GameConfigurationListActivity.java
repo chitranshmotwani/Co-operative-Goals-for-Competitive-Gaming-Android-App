1e9f1695cf6d81db7ebb9a652871123fc1b77715
trgeorge <trgeorg@sfu.ca>
2022-11-05 21:18:01 -0700

    test commit

--- app/src/main/java/ca/cmpt276/Calcium/GameConfigurationListActivity.java
+++ app/src/main/java/ca/cmpt276/Calcium/GameConfigurationListActivity.java
@@ -2,7 +2,14 @@ package ca.cmpt276.Calcium;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class GameConfigurationListActivity extends AppCompatActivity {
 
@@ -10,5 +17,40 @@ public class GameConfigurationListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = findViewById(R.id.fab_1);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(GameConfigurationListActivity.this, GameConfigurationActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        populateList();
+        registerClick();
+
+
+    }
+
+    private void populateList() {
+
+        String[] gameConfigs = {"1","2","3"}; //Here's where the array of game configs go
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.game_configs_layout, gameConfigs);
+
+        ListView list= findViewById(R.id.list_main);
+        list.setAdapter(adapter);
+    }
+    private void registerClick() {
+        ListView list= findViewById(R.id.list_main);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+
     }
 }
