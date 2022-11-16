ea64470c23d07dfa96ff2d4543a82843e6dce1b5
Weipeng Yao <weipengy@sfu.ca>
2022-11-16 08:54:19 -0800

    1.1 Individual player in UI

--- app/src/main/java/ca/cmpt276/Calcium/NewGameActivity.java
+++ app/src/main/java/ca/cmpt276/Calcium/NewGameActivity.java
@@ -2,18 +2,25 @@ package ca.cmpt276.Calcium;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TableLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
 
 import ca.cmpt276.Calcium.model.GameConfigManager;
 import ca.cmpt276.Calcium.model.GameConfiguration;
@@ -22,9 +29,11 @@ public class NewGameActivity extends AppCompatActivity {
 
     private int numOfPlayers;
     private int combinedScores;
+    private int currNumOfPlayers = 1;
     private GameConfigManager manager;
     private GameConfiguration gameConfig;
     private Menu optionsMenu;
+    ArrayList<Integer> scoreList = new ArrayList<>();
     private int index = 0;
     private boolean playersChanged = false;
     private boolean scoreChanged = false;
@@ -35,14 +44,28 @@ public class NewGameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_new_game);
         Intent in = getIntent();
         index = in.getIntExtra("passing selected gameConfig", 0);
+        LinearLayout scoreListView = findViewById(R.id.individual_score_list);
+        Button add_player = findViewById(R.id.add_player);
+        EditText currentPlayerScore = findViewById(R.id.player1_score);
 
         manager = GameConfigManager.getInstance(null);
         gameConfig = manager.getConfig(index);
         setTitle(gameConfig.getName());
 
+        add_player.setOnClickListener( view -> {
+            if (currNumOfPlayers+1 <= numOfPlayers) {
+                currNumOfPlayers++;
+                addOnePlayer(scoreListView);
+            }
+            else {
+                Toast.makeText(this, "Cannot add more players.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
         setupGameScoreDescription();
         setupGameNumPlayersTextWatcher();
         setupGameCombinedScoreTextWatcher();
+        setupPlayerScoreListTextWatcher(currentPlayerScore);
     }
 
     private void setupGameScoreDescription() {
@@ -86,6 +109,44 @@ public class NewGameActivity extends AppCompatActivity {
                 combinedScores = Integer.parseInt(String.valueOf(combinedScore.getText()));
             }
         });
+    }
+
+    private void setupPlayerScoreListTextWatcher(EditText score) {
+        score.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                scoreList.add(Integer.parseInt(String.valueOf(score.getText())));
+            }
+        });
+    }
+
+    private void addOnePlayer(LinearLayout scoreListView){
+        TextView addPlayerTitle = new TextView(this);
+        EditText addPlayerScore = new EditText(this);
+
+        addPlayerTitle.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.WRAP_CONTENT,1.0f));
+        addPlayerTitle.setText("Player"+currNumOfPlayers);
+        addPlayerTitle.setTextColor(Color.WHITE);
+        addPlayerTitle.setTextSize(20);
+        addPlayerTitle.setTextAlignment(TextView.TEXT_ALIGNMENT_CENTER);
+        addPlayerScore.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.WRAP_CONTENT,1.0f));
+        addPlayerScore.setTextColor(Color.WHITE);
+        addPlayerScore.setTextAlignment(TextView.TEXT_ALIGNMENT_CENTER);
+        addPlayerScore.setHintTextColor(Color.parseColor("#6FFFEB3B"));
+        addPlayerScore.setHint("Please enter the score of player" + currNumOfPlayers);
+        scoreListView.addView(addPlayerTitle);
+        scoreListView.addView(addPlayerScore);
+        setupPlayerScoreListTextWatcher(addPlayerScore);
     }
 
     @Override
