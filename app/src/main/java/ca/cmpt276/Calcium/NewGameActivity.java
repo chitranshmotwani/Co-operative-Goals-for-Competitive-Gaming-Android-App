09410768b1427f073eb3776709c573aac6f095c2
cma115 <chitransh_motwani@sfu.ca>
2022-11-18 03:21:37 -0800

    3 different theme options to select theme for achievements. Will visual improvements when doing issue #23 Friendly UI.

--- app/src/main/java/ca/cmpt276/Calcium/NewGameActivity.java
+++ app/src/main/java/ca/cmpt276/Calcium/NewGameActivity.java
@@ -13,11 +13,13 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -39,11 +41,13 @@ public class NewGameActivity extends AppCompatActivity {
     private Menu optionsMenu;
     private boolean playersChanged = false;
     private boolean scoreChanged = false;
+    private Spinner spThemes;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Utils.onActivityCreateSetTheme(this);
         setContentView(R.layout.activity_new_game);
         Intent in = getIntent();
         int index = in.getIntExtra("passing selected gameConfig", 0);
@@ -52,6 +56,7 @@ public class NewGameActivity extends AppCompatActivity {
         gameConfig = manager.getConfig(index);
         setTitle(gameConfig.getName());
 
+        setupSpinnerItemSelection();
         setupGameScoreDescription();
         setupGameNumPlayersTextWatcher();
         setupGameCombinedScoreTextWatcher();
@@ -176,6 +181,30 @@ public class NewGameActivity extends AppCompatActivity {
         return GameConfiguration.DifficultyLevel.NORMAL;
     }
 
+    private void setupSpinnerItemSelection() {
+        spThemes = (Spinner) findViewById(R.id.spThemes);
+        spThemes.setSelection(ThemeApplication.currentPosition);
+        ThemeApplication.currentPosition = spThemes.getSelectedItemPosition();
+
+        spThemes.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                if (ThemeApplication.currentPosition != position) {
+                    Utils.changeToTheme(NewGameActivity.this, position);
+                }
+                ThemeApplication.currentPosition = position;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+    }
+
     private void showAchievementLevelEarned() {
         GameConfiguration.Game newestGame = gameConfig.getGame(gameConfig.getNumOfGames() - 1);
         GameConfiguration.AchievementLevel lvl = newestGame.getAchievementLevel();
@@ -184,41 +213,80 @@ public class NewGameActivity extends AppCompatActivity {
         String difficulty = newestGame.getDifficultyLevel().toString().toLowerCase(Locale.ROOT);
         difficulty = difficulty.substring(0, 1).toUpperCase(Locale.ROOT) + difficulty.substring(1);
         String level = getString(manager.getLevelID(index));
+        if (ThemeApplication.currentPosition==0) {
+            MediaPlayer mp = MediaPlayer.create(this, R.raw.achievement_sound1);
+            mp.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mp) {
+                    mp.start();
+                }
+            });
 
-        MediaPlayer mp = MediaPlayer.create(this, R.raw.achievement_sound);
-        mp.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-            @Override
-            public void onPrepared(MediaPlayer mp) {
-                mp.start();
+            mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    mp.release();
+                }
+            });
+        }
+        else if (ThemeApplication.currentPosition==1) {
+            MediaPlayer mp = MediaPlayer.create(this, R.raw.achievement_sound2);
+            mp.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mp) {
+                    mp.start();
+                }
+            });
+
+            mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    mp.release();
+                }
+            });
+        }
+        else if (ThemeApplication.currentPosition==2){
+            MediaPlayer mp = MediaPlayer.create(this, R.raw.achievement_sound3);
+            mp.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mp) {
+                    mp.start();
+                }
+            });
+
+            mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    mp.release();
+                }
+            });
+        }
+            Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in);
+            String s = getString(R.string.achievement_name) + "\n" + level + getString(R.string.on_difficulty_lvl) + difficulty;
+            Dialog dialog = new Dialog(this);
+            if (ThemeApplication.currentPosition==0) {
+                dialog.setContentView(R.layout.popup_achievement1);
             }
-        });
-
-        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-            @Override
-            public void onCompletion(MediaPlayer mp) {
-                mp.release();
+            else if (ThemeApplication.currentPosition==1) {
+                dialog.setContentView(R.layout.popup_achievement2);
             }
-        });
-
-        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in);
-        String s = getString(R.string.achievement_name) + "\n" + level + getString(R.string.on_difficulty_lvl) + difficulty;
-        Dialog dialog = new Dialog(this);
-        dialog.setContentView(R.layout.popup_achievement);
-        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-        TextView tv = dialog.findViewById(R.id.achievement_name);
-        ImageView iv = dialog.findViewById(R.id.achievement_star);
-        iv.setAnimation(animation);
-        tv.setText(s);
-        tv.setAnimation(animation);
-        dialog.setCancelable(false);
-
-        Button popupBtn = dialog.findViewById(R.id.popup_ok_button);
-        popupBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
+            else if (ThemeApplication.currentPosition==2) {
+                dialog.setContentView(R.layout.popup_achievement3);
             }
-        });
-        dialog.show();
-    }
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            TextView tv = dialog.findViewById(R.id.achievement_name);
+            ImageView iv = dialog.findViewById(R.id.achievement_star);
+            iv.setAnimation(animation);
+            tv.setText(s);
+            tv.setAnimation(animation);
+            dialog.setCancelable(false);
+            Button popupBtn = dialog.findViewById(R.id.popup_ok_button);
+            popupBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    finish();
+                }
+            });
+            dialog.show();
+        }
 }
---
+++ app/src/main/java/ca/cmpt276/Calcium/Utils.java
@@ -0,0 +1,31 @@
+package ca.cmpt276.Calcium;
+
+import android.app.Activity;
+import android.content.Intent;
+
+public class Utils {
+    private static int sTheme;
+
+    public final static int THEME_1 = 0;
+    public final static int THEME_2 = 1;
+    public final static int THEME_3 = 2;
+
+    public static void changeToTheme(Activity activity, int theme) {
+        sTheme = theme;
+    }
+
+    public static void onActivityCreateSetTheme(Activity activity) {
+        switch (sTheme) {
+            default:
+            case THEME_1:
+                activity.setTheme(R.style.Theme1);
+                break;
+            case THEME_2:
+                activity.setTheme(R.style.Theme2);
+                break;
+            case THEME_3:
+                activity.setTheme(R.style.Theme3);
+                break;
+        }
+    }
+}
